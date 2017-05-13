058c4016044075f3c3ad2138c97b6a323c89131b
Erjan Maksatbekov <mambo1234554321@gmail.com>
2017-05-13 18:24:33 +0600

    УРААА Это слайдер наконечто закончен

--- js/core.js
+++ js/core.js
@@ -1,4 +1,48 @@
 $(document).ready(function(){
+		// Смотрит на ширину экрана при изменении расширении
+		$(window).resize(function() {
+			if($(this).width() > 1000) {
+				$('.list__check--drop').click(function(){
+					$('.drop-down').slideToggle(700);	
+					if ($(this).hasClass('not-active')) {
+						$(this).addClass('drop-down--active').removeClass('not-active');
+					}else{
+						setTimeout(function(){
+							$('.drop-down--active').addClass('not-active').removeClass('drop-down--active')
+						},600)			
+					}		
+				});
+			}
+			else {
+					$('#menu2').slicknav({
+						label: '',
+						duration: 1000,
+						brand: "<img src='img/logo.png'>"
+					});
+			}
+		})
 
+		if($(window).width() < 1000) {
+					$('#menu2').slicknav({
+						label: '',
+						duration: 1000,
+						brand: "<img src='img/logo.png'>"
+					});
+		}
+		else {
+			$('.list__check--drop').click(function(){
+					$('.drop-down').slideToggle(700);	
+					if ($(this).hasClass('not-active')) {
+						$(this).addClass('drop-down--active').removeClass('not-active');
+					}else{
+						setTimeout(function(){
+							$('.drop-down--active').addClass('not-active').removeClass('drop-down--active')
+						},600)			
+					}		
+				});
+		}
+		$('.slick').slick({
+				dots: true
+			});
 
-});
+	});
